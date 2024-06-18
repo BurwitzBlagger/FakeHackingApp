f3c5c47e61b72ddb95961086b655e4a958f90b9d
BurwitzBlagger <99707350+BurwitzBlagger@users.noreply.github.com>
2024-06-18 15:44:37 +0700

    Add files via upload

---
+++ hacksleeptest.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hack Engine v1.35 by Cudnamreg\n");
+    int ip = 0;
+    printf("Type the IPv4 address of the device you want to hack!\n");
+    scanf ("%d, ip");
+    Sleep(100);
+       printf("Verifying address... \n");
+           Sleep(1000);
+          printf("Tracing routers...\n");
+              Sleep(10000);
+             printf("Performing hops....\n");
+                 Sleep(10000);
+                       printf("Finding the address....\n");
+                           Sleep(10000);
+                          printf("Scanning for vulnerable ports.....\n");
+                              Sleep(10000);
+                             printf("Port found.\n");
+                                 Sleep(999);
+                             printf("Delivering packages...\n");
+                                 Sleep(10000);
+                                printf("Hack failed. Packages stopped by a resistant firewall or software.\n");
+    system ("PAUSE");
+    return 0;
+
+}
